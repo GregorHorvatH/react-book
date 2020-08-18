f1c618483fb55cfe90fb16f7b2c1c8109433cd07
Geri <gregor.horvath83@gmail.com>
2020-08-18 17:51:32 +0300

    update

--- src/pages/Lesson-15/index.js
+++ src/pages/Lesson-15/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import { CopyBlock, hybrid } from 'react-code-blocks';
-// import Link from '../../components/Link';
+import Image from '../../components/Image';
+import Link from '../../components/Link';
 
 // import * as code from './code';
 
@@ -8,7 +9,38 @@ const Lesson15 = () => (
   <div className="container lesson-15">
     <div className="row">
       <div className="col">
-        <h2>Lesson 15</h2>
+        <h2>1. JWT(JSON Web Token)</h2>
+        <Image filename="jwt.png" />
+
+        <h3>1.1. Дополнительные материалы</h3>
+        <ul>
+          <li>
+            <Link
+              link="https://jwt.io/introduction/"
+              text="Introduction to JSON Web Tokens"
+            />
+          </li>
+          <li>
+            <Link
+              link="https://gist.github.com/zmts/802dc9c3510d79fd40f9dc38a12bccfc"
+              text="Про токены, JSON Web Tokens (JWT), аутентификацию и авторизацию"
+            />
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div className="row">
+      <div className="col">
+        <h2>2. Авторизация и аутентификация пользователя</h2>
+        <Image filename="auth_process.png" />
+      </div>
+    </div>
+
+    <div className="row">
+      <div className="col">
+        <h2>3. Защищенные ресурсы</h2>
+        <Image filename="auth_diag.png" />
       </div>
     </div>
   </div>
